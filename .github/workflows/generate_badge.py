7ddb8b0d64618c6ec5713215c00ab8d83516dca8
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2024-05-15 09:31:25 +0200

    chore: drop codecov

---
+++ .github/workflows/generate_badge.py
@@ -0,0 +1,106 @@
+# ruff: noqa: E471
+import argparse
+import colorsys
+import re
+from pathlib import Path
+from typing import Optional, Tuple
+
+DEFAULT_TEMPLATE = """\
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="108" height="20" role="img" aria-label="coverage: 97.1%">
+    <title>coverage: {COVERAGE}</title>
+    <g shape-rendering="crispEdges">
+        <rect width="61" height="20" fill="#555"/>
+        <rect x="61" width="47" height="20" fill="{COLOR}"/>
+    </g>
+    <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
+        <text x="315" y="140" transform="scale(.1)" fill="#fff" textLength="510">coverage</text>
+        <text x="835" y="140" transform="scale(.1)" fill="#fff" textLength="{WIDTH}">{COVERAGE}</text>
+    </g>
+</svg>"""  # noqa: E501
+
+
+def parse_any_color_as_hsl(color: str) -> Tuple[float, float, float]:
+    if color.startswith("hsl"):
+        h, s, l = tuple(map(float, re.findall(r"\d+(?:\.\d+)?", color)))[:3]
+        return (h / 360, s / 100, l / 100)
+    elif color.startswith("#"):
+        rgb = tuple(int(color[i : i + 2], 16) / 255 for i in (1, 3, 5))
+        h, l, s = colorsys.rgb_to_hls(*rgb)
+    elif color.startswith("rgb"):
+        h, l, s = colorsys.rgb_to_hls(*map(int, re.findall(r"\d+", color)))
+    else:
+        raise ValueError(f"Unknown color format: {color}")
+    return h, s, l
+
+
+def make_badge(
+    bad_color_hsl: Tuple[float, float, float],
+    good_color_hsl: Tuple[float, float, float],
+    min_coverage: int = 70,
+    report_path: str = "coverage.txt",
+    badge_template_path: Optional[str] = None,
+):
+    with open(report_path) as f:
+        coverage_str = float(f.read().splitlines(False)[-1].split()[-1].strip(" %"))
+    coverage = float(coverage_str)
+    ratio = (max(coverage, min_coverage) - min_coverage) / (100 - min_coverage)
+    hue = bad_color_hsl[0] + ratio * (good_color_hsl[0] - bad_color_hsl[0])
+    saturation = bad_color_hsl[1] + ratio * (good_color_hsl[1] - bad_color_hsl[1])
+    lightness = bad_color_hsl[2] + ratio * (good_color_hsl[2] - bad_color_hsl[2])
+    rgb = colorsys.hls_to_rgb(hue, lightness, saturation)
+
+    badge_template = (
+        Path(badge_template_path).read_text()
+        if badge_template_path
+        else DEFAULT_TEMPLATE
+    )
+    badge = badge_template.format(
+        COVERAGE=f"{int(coverage)}%",
+        COLOR=f"#{''.join(f'{int(c * 255):02x}' for c in rgb)}",
+        WIDTH="250" if coverage < 100 else "330",
+    )
+    return badge
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-b",
+        "--bad-color",
+        type=str,
+        default="hsl(9.62deg 71.56% 57.25%)",
+    )
+    parser.add_argument(
+        "-g",
+        "--good-color",
+        type=str,
+        default="hsl(103.64deg 84.62% 43.33%)",
+    )
+    parser.add_argument(
+        "-m",
+        "--min-coverage",
+        type=int,
+        default=70,
+    )
+    parser.add_argument(
+        "-r",
+        "--report-path",
+        type=str,
+        default="coverage.txt",
+    )
+    parser.add_argument(
+        "-t",
+        "--badge-template-path",
+        type=str,
+        default=None,
+        nargs="?",
+    )
+    args = parser.parse_args()
+    badge = make_badge(
+        bad_color_hsl=parse_any_color_as_hsl(args.bad_color),
+        good_color_hsl=parse_any_color_as_hsl(args.good_color),
+        min_coverage=args.min_coverage,
+        report_path=args.report_path,
+        badge_template_path=args.badge_template_path,
+    )
+    print(badge)
