b7074edfbfa2eac76da07f229a36cf91ef7c6784
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2024-09-09 11:37:04 +0200

    chore: bump version to 0.3.5

--- foldedtensor/__init__.py
+++ foldedtensor/__init__.py
@@ -45,7 +45,7 @@ try:
 except AttributeError:
     DisableTorchFunctionSubclass = torch._C.DisableTorchFunction
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 
 class FoldedTensorLengths(UserList):
